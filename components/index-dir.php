eb31e3db9986bffed57bcb54b4adeef98d53c6d9
Dmitry <volcharo@gmail.com>
2024-02-05 13:43:29 +0300

    code

--- components/index-dir.php
+++ components/index-dir.php
@@ -1,7 +1,171 @@
 <!-- Направления -->
 
-<div class="container index-dir">
-    
+<div class="container index-dir my-5">
+    <div class="d-flex align-items-end">
+        <div class="py-2 pe-2">
+            <img src="./assets/img/dir.svg" alt="">
+        </div>
+        <h1 class="fw-normal text-style-blue">
+            Основные направления
+        </h1>
+    </div>
+
+    <div class="row g-4 my-4 index-dir-items">
+        <div class="col-md-6">
+            <div class="index-dir-item border-style-cyan border-2 rounded-5 p-4 h-100 d-flex flex-column">
+                <div class="d-flex p-2">
+                    <h4 class="text-style-darkblue lh-1 pe-2 flex-grow-1">
+                        Приемы детских специалистов
+                    </h4>
+                    <img src="./assets/img/spec-icons/spec-icons-6.svg" alt="">
+                </div>
+                <p class="text-style-darkblue lh-sm p-2">
+                    опытные и внимательные педиатры, неонатлоги, офтальмологи, отоларингологи и неврологи
+                </p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="index-dir-item border-style-cyan border-2 rounded-5 p-4 h-100 d-flex flex-column">
+                <div class="d-flex p-2">
+                    <h4 class="text-style-darkblue lh-1 pe-2 flex-grow-1">
+                        Программы
+                    </h4>
+                    <img src="./assets/img/spec-icons/spec-icons-19.svg" alt="">
+                </div>
+                <p class="text-style-darkblue lh-sm p-2">
+                    наблюдение детей от 0 до 18 лет
+                </p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="index-dir-item border-style-cyan border-2 rounded-5 p-4 h-100 d-flex flex-column">
+                <div class="d-flex p-2">
+                    <h4 class="text-style-darkblue lh-1 pe-2 flex-grow-1">
+                        Вакцинация
+                    </h4>
+                    <img src="./assets/img/spec-icons/spec-icons-18.svg" alt="">
+                </div>
+                <p class="text-style-darkblue lh-sm p-2">
+                    все вакцины в наличии
+                </p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="index-dir-item border-style-cyan border-2 rounded-5 p-4 h-100 d-flex flex-column">
+                <div class="d-flex p-2">
+                    <h4 class="text-style-darkblue lh-1 pe-2 flex-grow-1">
+                        Травмпункт 
+                    </h4>
+                    <img src="./assets/img/spec-icons/spec-icons-11.svg" alt="">
+                </div>
+                <p class="text-style-darkblue lh-sm p-2">
+                    срочная помощь в день обращения
+                </p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="index-dir-item border-style-cyan border-2 rounded-5 p-4 h-100 d-flex flex-column">
+                <div class="d-flex p-2">
+                    <h4 class="text-style-darkblue lh-1 pe-2 flex-grow-1">
+                    Вызов педиатрической бригады скорой помощи 
+                    </h4>
+                    <img src="./assets/img/spec-icons/spec-icons-16.svg" alt="">
+                </div>
+                <p class="text-style-darkblue lh-sm p-2">
+                    экстренные и плановые выезды в любой район города
+                </p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="index-dir-item border-style-cyan border-2 rounded-5 p-4 h-100 d-flex flex-column">
+                <div class="d-flex p-2">
+                    <h4 class="text-style-darkblue lh-1 pe-2 flex-grow-1">
+                        Стационар
+                    </h4>
+                    <img src="./assets/img/spec-icons/spec-icons.svg" alt="">
+                </div>
+                <p class="text-style-darkblue lh-sm p-2">
+                    дневной и круглосуточный
+                </p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="index-dir-item border-style-cyan border-2 rounded-5 p-4 h-100 d-flex flex-column">
+                <div class="d-flex p-2">
+                    <h4 class="text-style-darkblue lh-1 pe-2 flex-grow-1">
+                    Круглосуточный прием педиатра
+                    </h4>
+                    <img src="./assets/img/spec-icons/spec-icons-4.svg" alt="">
+                </div>
+                <p class="text-style-darkblue lh-sm p-2">
+                    лечение в амбулаторных и стационарных условиях
+                </p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="index-dir-item border-style-cyan border-2 rounded-5 p-4 h-100 d-flex flex-column">
+                <div class="d-flex p-2">
+                    <h4 class="text-style-darkblue lh-1 pe-2 flex-grow-1">
+                        УЗИ
+                    </h4>
+                    <img src="./assets/img/spec-icons/spec-icons-12.svg" alt="">
+                </div>
+                <p class="text-style-darkblue lh-sm p-2">
+                    сердце, нейросонография, почки, брюшная полость и т.п.
+                </p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="index-dir-item border-style-cyan border-2 rounded-5 p-4 h-100 d-flex flex-column">
+                <div class="d-flex p-2">
+                    <h4 class="text-style-darkblue lh-1 pe-2 flex-grow-1">
+                        Справки 
+                    </h4>
+                    <img src="./assets/img/spec-icons/spec-icons-10.svg" alt="">
+                </div>
+                <p class="text-style-darkblue lh-sm p-2">
+                    оформление справок за одно посещение
+                </p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="index-dir-item border-style-cyan border-2 rounded-5 p-4 h-100 d-flex flex-column">
+                <div class="d-flex p-2">
+                    <h4 class="text-style-darkblue lh-1 pe-2 flex-grow-1">
+                        Функциональная диагностика
+                    </h4>
+                    <img src="./assets/img/spec-icons/spec-icons-17.svg" alt="">
+                </div>
+                <p class="text-style-darkblue lh-sm p-2">
+                    ЭЭГ во сне, ЭКГ, Холтер
+                </p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="index-dir-item border-style-cyan border-2 rounded-5 p-4 h-100 d-flex flex-column">
+                <div class="d-flex p-2">
+                    <h4 class="text-style-darkblue lh-1 pe-2 flex-grow-1">
+                        Физиотерапия
+                    </h4>
+                    <img src="./assets/img/spec-icons/spec-icons-14.svg" alt="">
+                </div>
+                <p class="text-style-darkblue lh-sm p-2">
+                    УВЧ, магнитотерапия, КУФ, лазеротерапия, светотерапия
+                </p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="h-100 d-flex align-items-center justify-content-center">
+                <a href="" class="bg-style-red bg-gradient d-flex align-items-center rounded-5 py-2 px-5 text-white">
+                    <img src="./assets/img/logo_sml_white.svg" alt="" class="pe-2">
+                    <div>Все направления</div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+
 </div>
 
 <!-- /Направления -->
